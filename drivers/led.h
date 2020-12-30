8b105819cf93c2e3f1a3090e69da1c71204a42f4
Abe Rio <261507455@QQ.COM>
2020-12-30 22:19:22 +0800

    add some oled function

--- drivers/led.h
+++ drivers/led.h
@@ -2,7 +2,7 @@
 #define _LED_H
 
 #include "reg52.h"
-//模块上的LED接口，低电平导通
+//led pin
 #define LED P3^0
 
 void led_on(void);
---
+++ drivers/spi_oled.c
@@ -0,0 +1,118 @@
+#include "spi_oled.h"
+//OLED shows 
+//like this:
+//[0]0 1 2 3 ... 127	
+//[1]0 1 2 3 ... 127	
+//[2]0 1 2 3 ... 127	
+//[3]0 1 2 3 ... 127	
+//[4]0 1 2 3 ... 127	
+//[5]0 1 2 3 ... 127	
+//[6]0 1 2 3 ... 127	
+//[7]0 1 2 3 ... 127 
+
+
+void delay_ms(u32 ms)
+{                         
+	u32 a;
+	while(ms)
+	{
+		a=1800;
+		while(a--);
+		ms--;
+	}
+	return;
+}
+
+
+void OLED_WR_Byte(u8 dat,u8 cmd)
+{	
+	u8 i;			  
+	if(cmd)
+	  OLED_DC_Set();
+	else 
+	  OLED_DC_Clr();		  
+	OLED_CS_Clr();
+	for(i=0;i<8;i++)
+	{			  
+		OLED_SCL_Clr();
+		if(dat&0x80)
+		{
+		 OLED_SDA_Set();
+		}
+		else
+		{
+		 OLED_SDA_Clr();
+		}
+	  OLED_SCL_Set();
+		dat<<=1;   
+	}				 		  
+	OLED_CS_Set();
+	OLED_DC_Set();   	  
+} 
+
+//turn on oled
+void OLED_Display_On(void)
+{
+	OLED_WR_Byte(0X8D,OLED_CMD);  //SET DCDC command
+	OLED_WR_Byte(0X14,OLED_CMD);  //DCDC ON
+	OLED_WR_Byte(0XAF,OLED_CMD);  //DISPLAY ON
+}
+
+//turn off oled
+void OLED_Display_Off(void)
+{
+	OLED_WR_Byte(0X8D,OLED_CMD);  //SET DCDC command
+	OLED_WR_Byte(0X10,OLED_CMD);  //DCDC OFF
+	OLED_WR_Byte(0XAE,OLED_CMD);  //DISPLAY OFF
+}	
+
+//clear the oled,The effect is like an OLED without power
+void OLED_Clear(void)  
+{  
+	u8 i,n;		    
+	for(i=0;i<8;i++)  
+	{  
+		OLED_WR_Byte (0xb0+i,OLED_CMD);    //set page address（0~7）
+		OLED_WR_Byte (0x00,OLED_CMD);      //set show position—Column low address
+		OLED_WR_Byte (0x10,OLED_CMD);      //set show position—Column high address   
+		for(n=0;n<128;n++)
+			OLED_WR_Byte(0,OLED_DATA); 
+	} //update display
+}
+
+void OLED_Init(void)
+{
+	OLED_RES_Clr();
+  delay_ms(200);
+	OLED_RES_Set();
+	
+	OLED_WR_Byte(0xAE,OLED_CMD);//--turn off oled panel
+	OLED_WR_Byte(0x00,OLED_CMD);//---set low column address
+	OLED_WR_Byte(0x10,OLED_CMD);//---set high column address
+	OLED_WR_Byte(0x40,OLED_CMD);//--set start line address  Set Mapping RAM Display Start Line (0x00~0x3F)
+	OLED_WR_Byte(0x81,OLED_CMD);//--set contrast control register
+	OLED_WR_Byte(0xCF,OLED_CMD); // Set SEG Output Current Brightness
+	OLED_WR_Byte(0xA1,OLED_CMD);//--Set SEG/Column Mapping     0xa0左右反置 0xa1正常
+	OLED_WR_Byte(0xC8,OLED_CMD);//Set COM/Row Scan Direction   0xc0上下反置 0xc8正常
+	OLED_WR_Byte(0xA6,OLED_CMD);//--set normal display
+	OLED_WR_Byte(0xA8,OLED_CMD);//--set multiplex ratio(1 to 64)
+	OLED_WR_Byte(0x3f,OLED_CMD);//--1/64 duty
+	OLED_WR_Byte(0xD3,OLED_CMD);//-set display offset	Shift Mapping RAM Counter (0x00~0x3F)
+	OLED_WR_Byte(0x00,OLED_CMD);//-not offset
+	OLED_WR_Byte(0xd5,OLED_CMD);//--set display clock divide ratio/oscillator frequency
+	OLED_WR_Byte(0x80,OLED_CMD);//--set divide ratio, Set Clock as 100 Frames/Sec
+	OLED_WR_Byte(0xD9,OLED_CMD);//--set pre-charge period
+	OLED_WR_Byte(0xF1,OLED_CMD);//Set Pre-Charge as 15 Clocks & Discharge as 1 Clock
+	OLED_WR_Byte(0xDA,OLED_CMD);//--set com pins hardware configuration
+	OLED_WR_Byte(0x12,OLED_CMD);
+	OLED_WR_Byte(0xDB,OLED_CMD);//--set vcomh
+	OLED_WR_Byte(0x40,OLED_CMD);//Set VCOM Deselect Level
+	OLED_WR_Byte(0x20,OLED_CMD);//-Set Page Addressing Mode (0x00/0x01/0x02)
+	OLED_WR_Byte(0x02,OLED_CMD);//
+	OLED_WR_Byte(0x8D,OLED_CMD);//--set Charge Pump enable/disable
+	OLED_WR_Byte(0x14,OLED_CMD);//--set(0x10) disable
+	OLED_WR_Byte(0xA4,OLED_CMD);// Disable Entire Display On (0xa4/0xa5)
+	OLED_WR_Byte(0xA6,OLED_CMD);// Disable Inverse Display On (0xa6/a7) 
+	OLED_Clear();
+	OLED_WR_Byte(0xAF,OLED_CMD); /*display ON*/ 
+}
